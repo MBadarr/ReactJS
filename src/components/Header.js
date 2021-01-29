72ad385458b6d4678562f078a2674a854cb2655c
M.Badar <mbadar418@gmail.com>
2021-01-30 03:34:43 +0500

    R

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+    return(
+        <div className="nav">
+        <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Services</li>
+            <li>Contact</li>
+        </ul>      
+        </div>
+    );
+}
+
+export default Header;
